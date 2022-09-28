35ff238b25adabfa697b6d2757387f98dc57d624
teschiopol <paolo.concolato@eniac.it>
2022-09-28 16:13:42 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Learn Vue
+# 🚀 Learn Vue
+
+Basic demo and start to learn Vue
+
+## 💾 Install Vue
+
+Visit: https://vuejs.org/guide/quick-start.html
+
+## 👨🏻‍💻 Use
+
+First example in ``getStart``
+
+Note: I'll use Options API
---
+++ getStart/App.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    data() {
+      return {
+        count: 0,
+        titleClass: 'title'
+      }
+    },
+    methods:{
+      increment(){
+        this.count++
+      }
+    }
+  }
+</script>
+
+<template>
+  <button @click="increment" v-bind:class="titleClass">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+  button {
+    font-weight: bold;
+  }
+  .title{
+    color: lightskyblue;
+  }
+</style>
