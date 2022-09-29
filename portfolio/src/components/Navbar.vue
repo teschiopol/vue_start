54f63a8c6ee202402cc4d338a29a75fdbeeca341
teschiopol <paolo.concolato@eniac.it>
2022-09-29 16:00:59 +0200

    chore(portfolio): init

---
+++ portfolio/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar app color="#111111" dark flat class="px-8">
+    <v-btn icon>
+      <v-icon color="#FFFFFF">fas fa-dragon</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default { // eslint-disable-next-line
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ portfolio/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template >
+  <v-app :style="{background: $vuetify.theme.themes.dark.background}">
+    <Navbar />
+  </v-app>
+</template>
+
+<script>
+  import Navbar from "@/components/Navbar";
+
+  export default { // eslint-disable-next-line
+    name: 'Home',
+
+    components: {
+      Navbar,
+    },
+  }
+</script>
